cb17fc5d26d484f492965cc735bc5edbeb9b2f48
Khislatjon <valijonovxislatjon@gmail.com>
2022-03-07 14:05:07 +0500

    Problems solved: 12, 70, 205, 242, 268, 290, 344, 345, 349, 350, 383, 387, 2186, 2190, 2191

--- LeetCode.playground/Contents.swift
+++ LeetCode.playground/Contents.swift
@@ -43,7 +43,6 @@ import Cocoa
 // MARK: - 5. Longest Palindromic Substring
 
 func longestPalindrome(_ s: String) -> String {
-    
     return ""
 }
 
@@ -101,26 +100,15 @@ func longestPalindrome(_ s: String) -> String {
 
 // MARK: - 12. Integer to Roman
 
-func intToRoman(_ num: Int) -> String {
-    var resultStr = ""
-    let dict = [1: "I", 5: "V", 10: "X", 50: "L",
-                100: "C", 500: "D", 1000: "M"]
-    
-    var arr = [Int]()
-    var givenN = num
-    var counter = 1
-    
-    while givenN != 0 {
-        arr.insert((givenN % 10) * counter, at: 0)
-        givenN /= 10
-        counter *= 10
-    }
-    
-    print(arr)
-    return ""
-}
-
-let a = intToRoman(2751)
+//func intToRoman(_ num: Int) -> String {
+//    let M = ["", "M", "MM", "MMM"]
+//    let C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+//    let X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+//    let I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+//    return M[num/1000] + C[(num/100) % 10] + X[(num/10) % 10] + I[num%10]
+//}
+//let a = intToRoman(1687)
+//print(a)
 
 
 // MARK: - 14. Longest Common Prefix
@@ -217,6 +205,40 @@ let a = intToRoman(2751)
 //let clsum = threeSumClosest([-1,2,1,-4], 1)
 //print(clsum)
 
+
+// MARK: - 17. Letter Combinations of a Phone Number
+
+func letterCombinations(_ digits: String) -> [String] {
+    let digitsArr = Array(digits)
+    let ph: [Character: [String]] = [
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"],
+    ]
+    
+    var res = [String]()
+    
+    for (i, v) in digitsArr.enumerated() {
+        if i == 0 {
+            res.append(contentsOf: ph[v]!)
+        } else {
+            for i in 0..<res.count {
+                for p in ph[v]! {
+                    res[i] += p
+                }
+            }
+        }
+    }
+    return res
+}
+
+let com = letterCombinations("23")
+print(com)
 
 // MARK: - 18. 4Sum
 
@@ -374,7 +396,6 @@ let a = intToRoman(2751)
 
 // MARK: - 37. Sudoku Solver
 
-
 func solveSudoku(_ board: inout [[Character]]) {
         
 }
@@ -403,6 +424,7 @@ func solveSudoku(_ board: inout [[Character]]) {
 //    return arr.isEmpty ? 0 : arr[arr.count-1].count
 //}
 //print(lengthOfLastWord(" s  hello"))
+
 
 // MARK: - 66. Plus One
 
@@ -456,6 +478,33 @@ func solveSudoku(_ board: inout [[Character]]) {
 //}
 //
 //print(addBinary("11010","10011"))
+
+
+// MARK: - 70. Climbing Stairs
+
+//func climbStairs(_ n: Int) -> Int {
+//    if n == 1 {
+//        return 1
+//    }
+//    if n == 2 {
+//        return 2
+//    }
+//
+//    var lastNumber = 2
+//    var preLastNumber = 1
+//    var allWays = 0
+//
+//    for _ in 3...n {
+//        allWays = lastNumber + preLastNumber
+//        preLastNumber = lastNumber
+//        lastNumber = allWays
+//
+//    }
+//    return allWays
+//}
+//
+//let stair = climbStairs(5)
+//print(stair)
 
 
 // MARK: - 118. Pascal's Triangle
@@ -658,6 +707,216 @@ print(rob([2]))
 //}
 
 
+// MARK: - 205. Isomorphic Strings
+
+//func isIsomorphic(_ s: String, _ t: String) -> Bool {
+//    guard s.count == t.count else { return false }
+//
+//    let s = Array(s)
+//    let t = Array(t)
+//    var source = [Character: Character]()
+//    var target = [Character: Character]()
+//
+//    for (i, char) in s.enumerated() {
+//        let t = t[i]
+//        if source[char] == nil && target[t] == nil {
+//            source[char] = t
+//            target[t] = char
+//        } else if source[char] != t || target[t] != char {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//let iso = isIsomorphic("paper", "title")
+//print(iso)
+
+
+// MARK: - 242. Valid Anagram
+
+//func isAnagram(_ s: String, _ t: String) -> Bool {
+//    var sDic = [Character: Int]()
+//    var tDic = [Character: Int]()
+//
+//    for sChar in s {
+//        if sDic[sChar] == nil {
+//            sDic[sChar] = 1
+//        } else {
+//            sDic[sChar]! += 1
+//        }
+//    }
+//    for tChar in t {
+//        if tDic[tChar] == nil {
+//            tDic[tChar] = 1
+//        } else {
+//            tDic[tChar]! += 1
+//        }
+//    }
+//    return sDic == tDic
+//}
+//let isAnag = isAnagram("aa", "a")
+//print(isAnag)
+
+
+// MARK: - 268. Missing Number
+
+//func missingNumber(_ nums: [Int]) -> Int {
+//    // space - O(1), time - O(n)
+//    let sum = nums.reduce(0, +)
+//    let all = nums.count * (nums.count + 1) / 2
+//    return all - sum
+//}
+//
+//let missingN = missingNumber([3, 2, 0])
+//print(missingN)
+
+
+// MARK: - 290. Word Pattern
+
+//func wordPattern(_ pattern: String, _ s: String) -> Bool {
+//    let words = s.split(separator: " ")
+//    guard words.count == pattern.count else { return false }
+//
+//    var source = [Character: Substring]()
+//    var target = [Substring: Character]()
+//
+//    for (i, char) in pattern.enumerated() {
+//        let word = words[i]
+//        if source[char] == nil && target[word] == nil {
+//            source[char] = word
+//            target[word] = char
+//        } else if source[char] != word || target[word] != char {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//let wordP = wordPattern("abba", "dog dog dog dog")
+//print(wordP)
+
+
+// MARK: - 344. Reverse String
+
+//func reverseString(_ s: inout [Character]) {
+//    var i = 0
+//    var j = s.count-1
+//
+//    while i < j {
+//        let char = s[i]
+//        s[i] = s[j]
+//        s[j] = char
+//        i += 1
+//        j -= 1
+//    }
+//    print(s)
+//}
+//var arr: [Character] = ["H", "d"]
+//reverseString(&arr)
+
+
+// MARK: - 345. Reverse Vowels of a String
+
+//func reverseVowels(_ s: String) -> String {
+//    let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+//    var res = Array(s)
+//    var i = 0
+//    var j = s.count - 1
+//
+//    while i < j {
+//        if vowels.contains(res[i]) && vowels.contains(res[j]) {
+//            let char = res[i]
+//            res[i] = res[j]
+//            res[j] = char
+//            i += 1
+//            j -= 1
+//        } else if vowels.contains(res[i]) {
+//            j -= 1
+//        } else if vowels.contains(res[j]) {
+//            i += 1
+//        } else {
+//            i += 1
+//            j -= 1
+//        }
+//    }
+//    return String(res)
+//}
+//
+//let reverseV = reverseVowels("leotcede")
+//print(reverseV)
+
+
+// MARK: - 349. Intersection of Two Arrays
+
+//func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+//    let set1 = Set(nums1)
+//    let set2 = Set(nums2)
+//    return Array(set1.intersection(set2))
+//}
+//
+//let intersec = intersection([1,2,2,1], [2, 2])
+//print(intersec)
+
+
+// MARK: - 350. Intersection of Two Arrays II
+
+//func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+//    var result = [Int]()
+//    var nums = nums2
+//
+//    for el in nums1 {
+//        if nums.contains(el) {
+//            result.append(el)
+//            nums.remove(at: nums.firstIndex(of: el)!)
+//        }
+//    }
+//    return result
+//}
+//
+//let i = intersect([1,2], [1, 1])
+//print(i)
+
+
+// MARK: - 383. Ransom Note
+
+//func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+//    var source = magazine
+//    var word = ""
+//
+//    for char in ransomNote {
+//        if source.contains(char) {
+//            word.append(char)
+//            source.remove(at: source.firstIndex(of: char)!)
+//        }
+//    }
+//    return word == ransomNote
+//}
+//
+//let canConstr = canConstruct("aa", "aba")
+//print(canConstr)
+
+
+// MARK: - 387. First Unique Character in a String
+
+//func firstUniqChar(_ s: String) -> Int {
+//    var dict = [Character: Int]()
+//    for char in s {
+//        dict[char] = dict[char] != nil ? dict[char]! + 1 : 1
+//    }
+//
+//    for (i, char) in s.enumerated() {
+//        if dict[char] == 1 {
+//            return i
+//        }
+//    }
+//    return -1
+//}
+//
+//let uniqueChar = firstUniqChar("leetcodel")
+//print(uniqueChar)
+
+
 // MARK: - 771. Jewels and Stones
 
 //func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
@@ -669,3 +928,108 @@ print(rob([2]))
 //    }
 //    return count
 //}
+
+
+// MARK: - 2186. Minimum Number of Steps to Make Two Strings Anagram II
+
+//func minSteps(_ s: String, _ t: String) -> Int {
+//    var counter = [Character: Int]()
+//
+//    for char in s {
+//        counter[char, default: 0] += 1
+//    }
+//    for char in t {
+//        counter[char, default: 0] -= 1
+//    }
+//
+//    var res = 0
+//    for difference in counter.values {
+//        res += abs(difference)
+//    }
+//    return res
+//}
+//
+//let minStep = minSteps("leetcode", "coats")
+//print(minStep)
+
+
+// MARK: - 2190. Most Frequent Number Following Key In an Array
+
+//func mostFrequent(_ nums: [Int], _ key: Int) -> Int {
+//    var dict = [Int: Int]()
+//    for i in 0...nums.count - 2 {
+//        if nums[i] == key {
+//            if dict[nums[i+1]] == nil {
+//                dict[nums[i+1]] = 1
+//            } else {
+//                dict[nums[i+1]]! += 1
+//            }
+//        }
+//    }
+//
+//    for (k, v) in dict {
+//        if v == dict.values.max() {
+//            return k
+//        }
+//    }
+//    return 0
+//}
+//
+//let ab = mostFrequent([2,2,2,2,3], 2)
+//print(ab)
+
+
+// MARK: - 2191. Sort the Jumbled Numbers
+
+//func sortJumbled(_ mapping: [Int], _ nums: [Int]) -> [Int] {
+//    var obj = [Obj]()
+//    for el in nums {
+//        var element = el
+//        var multipler = 1
+//        var sum = 0
+//        while element > 9 {
+//            sum += mapping[element % 10] * multipler
+//            element /= 10
+//            multipler *= 10
+//        }
+//        sum += mapping[element % 10] * multipler
+//        let a = Obj(el: el, sum: sum)
+//        obj.append(a)
+//    }
+//
+//    var arr = [Int]()
+//    for ob in obj.sorted(by: { $0.sum < $1.sum }) {
+//        arr.append(ob.el)
+//    }
+//    return arr
+//}
+//
+//let c = sortJumbled([8,9,4,0,2,1,3,5,7,6], [991,338,38])
+//print(c)
+//
+//struct Obj {
+//    let el: Int
+//    let sum: Int
+//}
+
+
+// MARK: - 2192. All Ancestors of a Node in a Directed Acyclic Graph
+
+func getAncestors(_ n: Int, _ edges: [[Int]]) -> [[Int]] {
+    var arr = Array(repeating: [Int](), count: n)
+    for i in 0..<n {
+        for minArr in edges {
+            if minArr[1] == i {
+                arr[i].append(minArr[0])
+            }
+        }
+        for j in arr[i] {
+            arr[i].append(contentsOf: arr[j])
+            arr[i] = Array(Set(arr[i])).sorted()
+        }
+    }
+    return arr
+}
+
+let v = getAncestors(5, [[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]])
+print(v)
