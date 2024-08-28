64df37e7b7b474e68e2f8e7ec383b4e164af4df2
Khislatjon Valijonov <valijonovxislatjon@gmail.com>
2024-08-28 22:30:44 +0500

    Create file for 877. Stone Game problem

---
+++ LeetCode/Completed/Medium/Dynamic Programming/StoneGame.swift
@@ -0,0 +1,16 @@
+//
+//  StoneGame.swift
+//  LeetCode
+//
+//  Created by Khislatjon Valijonov on 27/08/24.
+//
+
+import Foundation
+
+// MARK: - 877. Stone Game
+
+class StoneGame {
+    func stoneGame(_ piles: [Int]) -> Bool {
+        true
+    }
+}
